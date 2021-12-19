c82757403eab3a7e43c2852d8e54a58b0be71815
Ben Zandonati <baz23@cam.ac.uk>
2021-12-19 22:15:47 +0000

    new

--- full_test.py
+++ full_test.py
@@ -16,6 +16,12 @@ path = "/media/pi/*/*"
 device_path = "/media/pi/*"
 device_list = []
 
+# Gpio pins for each button (from top to bottom)
+BUTTONS = [5, 6, 16, 24]
+
+# These correspond to buttons A, B, C and D respectively
+LABELS = ['A', 'B', 'C', 'D']
+
 def check_usb():
 	global device_list
 	device_list = []
@@ -23,15 +29,17 @@ def check_usb():
 		device_list.append(file)
 	print(f'Num of devices: {len(device_list)}')
 
-def eject_usb():
+def eject_usb(pin):
+	label = LABELS[BUTTONS.index(pin)]
+    print("Button press detected on pin: {} label: {}".format(pin, label))
 	if len(device_list) == 1:
 		print(f"Ejecting {device_list[0]}")
 		cmd = f"sudo umount {device_list[0]}"
 		os.system(cmd)
-		return []
+		global device_list
+		device_list = []
 	else: 
 		print("no device inserted")
-		return device_list
 
 def read_files():
 	file_list = []
@@ -43,11 +51,7 @@ def read_files():
 
 ### button setup
 
-# Gpio pins for each button (from top to bottom)
-BUTTONS = [5, 6, 16, 24]
 
-# These correspond to buttons A, B, C and D respectively
-LABELS = ['A', 'B', 'C', 'D']
 
 # Set up RPi.GPIO with the "BCM" numbering scheme
 GPIO.setmode(GPIO.BCM)
@@ -56,20 +60,11 @@ GPIO.setmode(GPIO.BCM)
 # with a "PULL UP", which weakly pulls the input signal to 3.3V.
 GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-# "handle_button" will be called every time a button is pressed
-# It receives one argument: the associated input pin.
-def handle_button(pin):
-    label = LABELS[BUTTONS.index(pin)]
-    print("Button press detected on pin: {} label: {}".format(pin, label))
-    if label == 'D':
-    	device_list = eject_usb()
-
 # Loop through out buttons and attach the "handle_button" function to each
 # We're watching the "FALLING" edge (transition from 3.3V to Ground) and
 # picking a generous bouncetime of 250ms to smooth out button presses.
-for pin in BUTTONS:
-    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=250)
+
+GPIO.add_event_detect(BUTTONS[0], GPIO.FALLING, eject_usb, bouncetime=250)
 
 
 ### MAIN:
@@ -84,6 +79,7 @@ while True:
 		image_list = read_files()
 		while len(device_list) == 1:
 			print('testing loop')
+			print(device_list)
 			time.sleep(8)
 
 
