d82b1ca7ad29e0a1fead4fc0d90ae04efc82e8be
Ben Zandonati <baz23@cam.ac.uk>
2021-12-19 21:14:37 +0000

    new

--- full_test.py
+++ full_test.py
@@ -8,25 +8,35 @@ import random
 from inky import Inky7Colour as Inky
 from PIL import Image
 
-# inky = Inky()
-
-print("insert usb stick")
-time.sleep(10)
 print("starting")
 
-# Get the list of all files and directories
 path = "/media/pi/*/*"
-
-check_path = "/media/pi/*"
-
-# file_list = []
-# for file in glob.iglob(path, recursive=True):
-# 	file_list.append(file)
-
+device_path = "/media/pi/*"
+usb_inserted = False
 device_list = []
-for file in glob.iglob(check_path, recursive=False):
-	device_list.append(file)
+
+def check_usb():
+	
+	for file in glob.iglob(device_path, recursive=False):
+		device_list.append(file)
+
+	usb_inserted = False
+	print(len(device_list))
+	if len(device_list) == 1:
+		usb_inserted = True
+
+while not usb_inserted:
+	print("Please insert USB device")
+	time.sleep(5)
+	check_usb()
+
+print("Device inserted, ejecting")
+
+cmd = f"sudo umount {device_list[0]}"
+os.system(cmd)
+
+time.sleep(4)
+
+check_usb()
 
 
-# print(file_list)
-print(device_list)
