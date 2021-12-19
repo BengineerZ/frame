f359c481f086223dad52a87e1d131252528f5936
Ben Zandonati <baz23@cam.ac.uk>
2021-12-19 21:17:52 +0000

    new

--- full_test.py
+++ full_test.py
@@ -16,21 +16,23 @@ usb_inserted = False
 device_list = []
 
 def check_usb():
-	
+	device_list = []
 	for file in glob.iglob(device_path, recursive=False):
 		device_list.append(file)
 
-	usb_inserted = False
+	
 	print(len(device_list))
 	if len(device_list) == 1:
-		usb_inserted = True
+		return True
+	return False
 
 while not usb_inserted:
 	print("Please insert USB device")
 	time.sleep(5)
-	check_usb()
+	usb_inserted = check_usb()
 
 print("Device inserted, ejecting")
+print(device_list[0])
 
 cmd = f"sudo umount {device_list[0]}"
 os.system(cmd)
